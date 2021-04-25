5876b7485dd7f3ddb8fc1adbec2c07449eabe96b
sntkgz <senatkgz492@gmail.com>
2021-04-25 23:36:44 +0300

    update maın dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            child: Text('sena tokgoz'),
+            child: Text('sena tokgoz2'),
           ),
         ),
       ),
